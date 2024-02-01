ae57e2aa2bf00d663d3fd652a9501daa45284428
Douglas Land <dland@homestoryrewards.com>
2024-02-01 10:24:20 -0600

    upgraded modules, removed gittoslack integrations

--- go.mod
+++ go.mod
@@ -1,32 +1,37 @@
 module github.com/looprock/slack-gw-go
 
-go 1.19
+go 1.21
 
 require (
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
-	github.com/rs/zerolog v1.29.0
-	github.com/slack-go/slack v0.12.1
-	github.com/spf13/viper v1.16.0
-	go.uber.org/automaxprocs v1.5.2
+	github.com/rs/zerolog v1.31.0
+	github.com/slack-go/slack v0.12.3
+	github.com/spf13/viper v1.18.2
+	go.uber.org/automaxprocs v1.5.3
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/spf13/afero v1.9.5 // indirect
-	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- slack-gw.go
+++ slack-gw.go
@@ -3,16 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/rs/zerolog"
-	"github.com/slack-go/slack"
-	_ "go.uber.org/automaxprocs"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
-	"regexp"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/rs/zerolog"
+	"github.com/slack-go/slack"
 	"github.com/spf13/viper"
-	"github.com/go-ozzo/ozzo-validation"
+	_ "go.uber.org/automaxprocs"
 )
 
 var logger = zerolog.New(os.Stdout).With().Timestamp().Logger().Level(zerolog.InfoLevel)
@@ -21,8 +19,7 @@ var Config appConfig
 type appConfig struct {
 	Token string `mapstructure:token`
 	Debug string `mapstructure:debug`
-	Port string `mapstructure:port`
-	GTSUrl string `mapstructure:gtsurl`
+	Port  string `mapstructure:port`
 }
 
 type inputStruct struct {
@@ -32,84 +29,27 @@ type inputStruct struct {
 	Attachment string
 }
 
-type gittoSlackResponse struct {
-	GithubID string `json:"github_id"`
-	SlackID  string `json:"slack_id"`
-}
-
 func (config appConfig) Validate() error {
-    return validation.ValidateStruct(&config,
-        validation.Field(&config.Token, validation.Required),
-        validation.Field(&config.Port, validation.Required),
-        validation.Field(&config.GTSUrl, validation.Required),
-    )
+	return validation.ValidateStruct(&config,
+		validation.Field(&config.Token, validation.Required),
+		validation.Field(&config.Port, validation.Required),
+	)
 }
 
 func LoadConfig() error {
-    v := viper.New()
+	v := viper.New()
 	v.SetEnvPrefix("slackgw")
 	v.AutomaticEnv()
 	v.BindEnv("debug")
 	v.BindEnv("token")
-	v.BindEnv("gtsurl")
 	v.BindEnv("port")
 	v.SetDefault("port", 8080)
 
-    if err := v.Unmarshal(&Config); err != nil {
-        return err
-    }
-
-    return Config.Validate()
-}
-
-
-func returnSlackID(userID string) string {
-	urlString := fmt.Sprintf("%s?github_id=eq.%s", Config.GTSUrl, userID)
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", urlString, nil)
-	req.Header.Add("Accept", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		logMsg := fmt.Sprintf("slack-gw ERROR: Errored when sending request to %s: %s", urlString, req.Body)
-		logger.Error().Msg(logMsg)
+	if err := v.Unmarshal(&Config); err != nil {
+		return err
 	}
-	defer resp.Body.Close()
-	resp_body, _ := ioutil.ReadAll(resp.Body)
-	var response []gittoSlackResponse
-	err = json.Unmarshal([]byte(resp_body), &response)
-	if err != nil {
-		logger.Fatal().Err(err)
-	}
-	if len(response) != 1 {
-		return userID
-	}
-	rv := fmt.Sprintf("<@%s>", response[0].SlackID)
-	return rv
-}
 
-func gitToSlack(message string) string {
-	xv := strings.Split(message, " ")
-	var tmpout []string
-	for _, b := range xv {
-		var r string
-		var cv []string
-		var cc []string
-		obj, err := regexp.Match("gittoslack:.*", []byte(b))
-		if err != nil {
-			logger.Fatal().Err(err)
-		}
-		if obj {
-			cc = strings.Split(b, ":")
-			// commas are an issue
-			cv = strings.Split(cc[1], ",")
-			r = returnSlackID(cv[0])
-			tmpout = append(tmpout, r)
-		} else {
-			tmpout = append(tmpout, b)
-		}
-	}
-	reconstructedString := strings.Join(tmpout, " ")
-	return reconstructedString
+	return Config.Validate()
 }
 
 func returnMessage(messageFormat string, origMessage string, origTopic string) string {
@@ -172,8 +112,6 @@ func postMessage(messageFormat string, channelID string, inputMessage string) {
 	var err error
 	var timestamp string
 
-	inputMessage = gitToSlack(inputMessage)
-
 	if messageFormat == "markdown" {
 		textBlockObject := slack.NewTextBlockObject("mrkdwn", inputMessage, false, false)
 		sectionBlock := slack.NewSectionBlock(textBlockObject, nil, nil)
@@ -195,6 +133,10 @@ func postMessage(messageFormat string, channelID string, inputMessage string) {
 
 func main() {
 	LoadConfig()
+	if Config.Token == "" {
+		logger.Error().Msg("Slack token not set")
+		os.Exit(1)
+	}
 
 	if Config.Debug != "" {
 		logger = zerolog.New(os.Stdout).With().Timestamp().Logger().Level(zerolog.DebugLevel)
